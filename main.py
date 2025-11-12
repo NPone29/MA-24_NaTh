dc5435a698de239fcb08d116458e31a4c78f57a1
NPone29 <natan.humblet@gmail.com>
2025-11-12 14:26:29 +0100

    New version

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
-print("hello_world")
-print("hello_test")
+import pygame
+
+grid = [
+    [None, None, None, None, None, None, None, None],
+    [None, None, None, None, None, None, None, None],
+    [None, None, None, None, None, None, None, None],
+    [None, None, None, None, None, None, None, None],
+    [None, None, None, None, None, None, None, None],
+    [None, None, None, None, None, None, None, None],
+    [None, None, None, None, None, None, None, None],
+    [None, None, None, None, None, None, None, None],
+]
+
+running = True
+while running:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill(WHITE)
+    #draw_grid()
+    pygame.display.flip

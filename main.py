003b73c58ca8f61c75110d748c561bb868327057
NPone29 <natan.humblet@gmail.com>
2025-11-12 14:32:08 +0100

    First version

--- main.py
+++ main.py
@@ -16,6 +16,12 @@ Player_2 = 1
 
 current_player = Player_1
 
+def get_next_player(current_player):
+    if current_player == Player_1:
+        return Player_2
+    else :
+        return Player_1
+
 running = True
 while running:
 

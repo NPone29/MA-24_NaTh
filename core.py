992700a510e926e90696415e20d7a8b62c01900f
pj43svh <pj43svh@eduvaud.ch>
2025-11-19 15:35:32 +0100

    BETA Version 1.0.6

---
+++ core.py
@@ -0,0 +1,14 @@
+grid = [
+    [None, None, None, None, None, None, None, None],
+    [None, None, None, None, None, None, None, None],
+    [None, None, None, None, None, None, None, None],
+    [None, None, None, 0, 1, None, None, None],
+    [None, None, None, 1, 0, None, None, None],
+    [None, None, None, None, None, None, None, None],
+    [None, None, None, None, None, None, None, None],
+    [None, None, None, None, None, None, None, None],
+]
+
+BOARD_WIDTH = 8
+BOARD_HEIGHT =8
+TILE_SIZE = 100

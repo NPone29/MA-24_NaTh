2f834d57c783418a0c0670b7fd54ab155afa4fac
pj43svh <pj43svh@eduvaud.ch>
2025-11-26 15:46:04 +0100

    adding the new flowery background

--- gfx.py
+++ gfx.py
@@ -14,7 +14,7 @@ GREEN =(20, 163, 58)
 screen = pygame.display.set_mode([core.BOARD_WIDTH * core.TILE_SIZE, core.BOARD_HEIGHT * core.TILE_SIZE])
 pygame.display.set_caption("MA-24 : Othello game")
 
-background = pygame.image.load("Assets/background.png")  # on charge l'arrière plan (code donné par copilot)
+background = pygame.image.load("Assets/flowery_background.png")  # on charge l'arrière plan (code donné par copilot)
 background = pygame.transform.scale(background, (800, 800))
 
 gameIcon = pygame.image.load("Assets/icon.png")
